9b503323bb213ce7c8a4d51f141bffeda59cee30
ntdnguyendong <ntdnguyendong@gmail.com>
2021-06-03 17:52:46 +0700

    best score

--- assets/Scripts/GameManagaer.js
+++ assets/Scripts/GameManagaer.js
@@ -26,7 +26,7 @@ cc.Class({
         mainGame: cc.Node,
         block: cc.Prefab,
         score: cc.Label,
-        recored: cc.Label,
+        record: cc.Label,
         winGame: cc.Node,
         loseGame: cc.Node,
         _isChange: false,
@@ -36,6 +36,7 @@ cc.Class({
     onLoad() {
         this.initObj();
         this.eventHandler();
+        this.getScoreStorge();
     },
 
     initObj() {
@@ -260,6 +261,7 @@ cc.Class({
                 }
             }
         }
+        this.checkScore();
     },
 
     clickRestart() {
@@ -333,6 +335,23 @@ cc.Class({
         }
         if(this._isChange) {
             this.addNum();
+        }
+    },
+
+    getScoreStorge() {
+        let scoreStorge = cc.sys.localStorage.getItem('bestScore');
+        if (scoreStorge !== null) {
+            this.record.string = JSON.parse(scoreStorge);
+        } else {
+            this.record.string = 0;
+        }
+    },
+
+    checkScore() {
+        let newScore = parseInt(this.score.string);
+        if (newScore > this.record.string) {
+            cc.sys.localStorage.setItem('bestScore', JSON.stringify(newScore));
+            this.record.string = newScore;
         }
     },
 
